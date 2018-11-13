25a0ad2ccdb1b2a9d5c4997f882fad31ec955d70
Anwesh43 <anweshthecool0@gmail.com>
2018-11-13 10:41:12 +0530

    adding HorizBarRotStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhorizbarrotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhorizbarrotstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhorizbarrotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.horizbarrotstepview.HorizBarRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : HorizBarRotStepView = HorizBarRotStepView.create(this)
     }
 }
