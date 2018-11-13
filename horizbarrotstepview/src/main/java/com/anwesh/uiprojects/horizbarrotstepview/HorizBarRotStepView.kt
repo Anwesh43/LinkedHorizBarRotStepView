a9ba56e51c130078f3829ec25c4f9348db13e759
Anwesh43 <anweshthecool0@gmail.com>
2018-11-13 09:44:36 +0530

    adding HorizBarRotStepView class

--- horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
+++ horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
@@ -20,3 +20,20 @@ val SC_GAP : Float = 0.1f / PARTS
 val SIZE_FACTOR : Int = 3
 val STROKE_FACTOR : Int = 60
 
+class HorizBarRotStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
