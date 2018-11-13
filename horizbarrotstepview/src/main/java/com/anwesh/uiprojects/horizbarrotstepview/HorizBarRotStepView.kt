a00070c0a4069a162b90c44f7e8bb4397df70f50
Anwesh43 <anweshthecool0@gmail.com>
2018-11-13 10:00:14 +0530

    adding Float and Int extension methods

--- horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
+++ horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
@@ -20,6 +20,14 @@ val SC_GAP : Float = 0.1f / PARTS
 val SIZE_FACTOR : Int = 3
 val STROKE_FACTOR : Int = 60
 
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.getScaleFactor() : Float = Math.floor(this / PARTS.getInverse().toDouble()).toFloat()
+
+fun Float.updateScale(dir : Float) : Float = SC_GAP * dir * ((1 - getScaleFactor()) * rects.getInverse() + getScaleFactor())
+
 class HorizBarRotStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
