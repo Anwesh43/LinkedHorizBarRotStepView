4ac5a01fc605ffbe8c88a9394cd0d7df9eb775a3
Anwesh43 <anweshthecool0@gmail.com>
2018-11-13 10:21:21 +0530

    adding HBRSNode class

--- horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
+++ horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
@@ -122,4 +122,49 @@ class HorizBarRotStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HBRSNode(var i : Int, val state : State = State()) {
+        private var prev : HBRSNode? = null
+
+        private var next : HBRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HBRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHBRSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HBRSNode {
+            var curr : HBRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
