ba8312de60ed56321ff4ebeb42adea09946cb412
Anwesh43 <anweshthecool0@gmail.com>
2018-11-13 09:41:34 +0530

    adding libraries and defining constants

--- horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
+++ horizbarrotstepview/src/main/java/com/anwesh/uiprojects/horizbarrotstepview/HorizBarRotStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.horizbarrotstepview
 /**
  * Created by anweshmishra on 13/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val rects : Int = 3
+val PARTS : Int = 2
+val SC_GAP : Float = 0.1f / PARTS
+val SIZE_FACTOR : Int = 3
+val STROKE_FACTOR : Int = 60
+
